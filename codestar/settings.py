25938c9b5713358550f7a87d6f8bc4c2d1ecf133
Ibrahim <isa@gmail.com>
2025-07-23 21:38:45 +0200

    Fix ALLOWED_HOST for local deve,add new path to urls.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3i0@ym31@t^kuv$m+*hve=u49!*7s@0y*=67me)aa-m$+=+ll5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
 
 
 # Application definition
