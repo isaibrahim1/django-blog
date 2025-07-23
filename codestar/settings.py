56b0d11d130ada37126df8e09b2a35a3747dc34d
Ibrahim <isa@gmail.com>
2025-07-23 20:20:50 +0200

    Add Procfile and update settings.py for Heroku deployment (DEBUG=False, ALLOWED_HOSTS)

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3i0@ym31@t^kuv$m+*hve=u49!*7s@0y*=67me)aa-m$+=+ll5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
